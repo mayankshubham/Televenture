24635549612309ebd33219392d39a0f44d213ddc
Shubham Khatri <mayank.shubham94@gmail.com>
2019-05-12 19:24:46 +0530

    Investor page (#3)
    * about and investor page layout design
    
    * update title of pages
    
    * Investor Page
    
    * Code refactoring
    
    * Update height of cover image

---
+++ src/components/Investors/InvestorImage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
+
+const InvestorImage = ({ className, children, style, tag = 'section' }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "Televenture-Investor.webp" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 500) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  const imageData = data.desktop.childImageSharp.fluid
+  return (
+    <Image
+      Tag={tag}
+      style={style}
+      className={className}
+      fluid={imageData}
+    >
+      {children}
+    </Image>
+  )
+}
+
+export default InvestorImage
---
+++ src/components/Investors/Table.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import styles from './index.module.scss'
+import classNames from 'classnames'
+import moment from 'moment'
+
+const InvestorsTable = props => {
+  const [selectedRow, setSelectedRow] = useState('')
+
+  const { tableData } = props
+  return (
+    <table className={styles.eventsInfoTable}>
+      <thead>
+        <tr>
+          <th>INVESTOR EVENT</th>
+          <th>DATE</th>
+          <th>VENUE</th>
+          <th>TIME</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map(obj => {
+          const { eventName, venue, date, startTime, endTime, id } = obj
+          return (
+            <tr
+              className={classNames({
+                [styles.selected]: selectedRow === id,
+              })}
+              onClick={() => setSelectedRow(id)}
+              key={`eventData-${id}`}
+            >
+              <td>{eventName}</td>
+              <td>{moment(date).format('MMMM D, YYYY')}</td>
+              <td>{venue}</td>
+              <td>{`${startTime} - ${endTime}`}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default InvestorsTable
