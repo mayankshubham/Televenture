d30c335654c7804e15f62a4fa014be83648a0b78
Shubham <shubhamkh@flock.com>
2019-05-14 00:29:45 +0530

    Added fund description

--- src/components/Funds/Card.js
+++ src/components/Funds/Card.js
@@ -2,10 +2,10 @@ import React from 'react'
 import styles from './card.module.scss'
 
 const Card = ({ details }) => {
-  const { title, founded, sector } = details
+  const { title, founded, sector, description, orgNo, url } = details
   return (
     <div className={styles.cardWrapper}>
-      <div className={styles.cardBody}>
+      <div className={styles.cardContent}>
         <div className={styles.cardHeader}>
           <h4 className={styles.title}>{title}</h4>
           <div className={styles.field}>
@@ -17,6 +17,28 @@ const Card = ({ details }) => {
               <span className={styles.heading}>Sector:</span>
               <span className={styles.value}>{sector}</span>
             </div>
+          </div>
+        </div>
+        <div className={styles.cardBody}>
+          <div className={styles.cardData}>
+            <div className={styles.description}>
+              {description.description ? (
+                <p>BUSINESS DESCRIPTION: {description.description}</p>
+              ) : null}
+            </div>
+            <div className={styles.cardFooter}>
+              <h4>
+                <span className={styles.heading}>ORG.NO:</span>
+                <span className={styles.orgValue}>{orgNo}</span>
+              </h4>
+              <h4>
+                <span className={styles.heading}>Website:</span>{' '}
+                <a href={url}>{url}</a>
+              </h4>
+            </div>
+          </div>
+          <div className={styles.heroImageContainer}>
+            <div className={styles.heroImage} />
           </div>
         </div>
       </div>
