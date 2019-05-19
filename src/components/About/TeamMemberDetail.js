b2defa20c3731820b99d305e5d07e2020b5b73ac
Shubham <shubhamkh@flock.com>
2019-05-19 23:33:19 +0530

    WIP

--- src/components/About/TeamMemberDetail.js
+++ src/components/About/TeamMemberDetail.js
@@ -1,25 +1,25 @@
-import React from "react";
-import styles from "./teamMemberDetail.module.scss";
-import classNames from "classnames";
-import Image from "gatsby-image";
+import React from 'react';
+import classNames from 'classnames';
+import Image from 'gatsby-image';
+import styles from './teamMemberDetail.module.scss';
 
 const colors = [
   {
-    backgroundColor: "#d2a756",
-    color: "#EEF3F6"
+    backgroundColor: '#d2a756',
+    color: '#EEF3F6',
   },
   {
-    backgroundColor: "#b4cad6",
-    color: "#40667C"
+    backgroundColor: '#b4cad6',
+    color: '#40667C',
   },
   {
-    backgroundColor: "#ffffff",
-    color: "#BC9B5D"
+    backgroundColor: '#ffffff',
+    color: '#BC9B5D',
   },
   {
-    backgroundColor: "#72a0ba",
-    color: "#F9F9F9"
-  }
+    backgroundColor: '#72a0ba',
+    color: '#F9F9F9',
+  },
 ];
 
 const TeamMemberDetail = props => {
@@ -27,19 +27,28 @@ const TeamMemberDetail = props => {
   const colorData = colors[index % 4];
   return (
     <div
-      style={{ backgroundColor: colorData.backgroundColor }}
       className={classNames(styles.teamMemberContainer, {
         [styles.left]: index % 2 === 0,
-        [styles.right]: index % 2 === 1
+        [styles.right]: index % 2 === 1,
       })}
     >
-      <Image tag="div" fluid={heroImage.fluid} className={styles.image} />
-      <div className={styles.description}>
-        <h6 className={styles.name}>{name}</h6>
-        <h6 style={{ color: colorData.color }} className={styles.designation}>
-          {designation}
-        </h6>
-        <p className={styles.info}>{selfDescription.selfDescription}</p>
+      <div
+        style={{
+          backgroundColor: colorData.backgroundColor,
+          position: 'relative',
+        }}
+      >
+        <Image tag="div" fluid={heroImage.fluid} className={styles.image} />
+        <div className={styles.iconContainer} />
+        <div className={styles.description}>
+          <div className={styles.header}>
+            <h6 className={styles.name}>{name}</h6>
+            <h6 style={{ color: colorData.color }} className={styles.designation}>
+              {designation}
+            </h6>
+          </div>
+          <p className={styles.info}>{selfDescription.selfDescription}</p>
+        </div>
       </div>
     </div>
   );
