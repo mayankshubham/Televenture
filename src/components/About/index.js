270d19551e0a1f87161e7f2b9ea53e1f08cce9b6
Shubham <shubhamkh@flock.com>
2019-05-20 13:16:08 +0530

    wip

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react';
-import classNames from 'classnames';
-import { Link, useStaticQuery, graphql } from 'gatsby';
-import Background from './BackgroundSection';
-import styles from './index.module.scss';
-import TeamMemberDetail from './TeamMemberDetail';
+import React, { useEffect } from "react";
+import classNames from "classnames";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import Background from "./BackgroundSection";
+import TeamMembersBackground from "./TeamMembersImage";
+import styles from "./index.module.scss";
+import TeamMemberDetail from "./TeamMemberDetail";
 
 const About = () => {
   const onScroll = () => {
     const scroll = window.pageYOffset;
-    const transform = `translate3d(-50%, -${scroll / 100}%, 0) scale(${(100 + scroll / 5) / 100})`;
+    const transform = `scale(${(100 + scroll / 5) / 100})`;
     console.log(scroll, transform);
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', onScroll);
+    window.addEventListener("scroll", onScroll);
     return () => {
-      window.removeEventListener('scroll', onScroll);
+      window.removeEventListener("scroll", onScroll);
     };
   }, []);
   const data = useStaticQuery(graphql`
@@ -47,33 +48,45 @@ const About = () => {
       <Background className={styles.aboutPageBackground} content="ABOUT" />
       <section className={classNames(styles.description)}>
         <p className={styles.mainDescription}>
-          Televenture is an Oslo based Nordic Venture Capital firm and was established in 1993 by Rune Rinnan. During
-          the period from 1993-2010, Televenture managed 8 Telenor Venture funds. In 2010, Televenture and our
-          Technology Transfer Office (TTO) partners in Oslo, Bergen, and Trondheim, established Norsk
-          Innovasjonskapital.
+          Televenture is an Oslo based Nordic Venture Capital firm and was
+          established in 1993 by Rune Rinnan. During the period from 1993-2010,
+          Televenture managed 8 Telenor Venture funds. In 2010, Televenture and
+          our Technology Transfer Office (TTO) partners in Oslo, Bergen, and
+          Trondheim, established Norsk Innovasjonskapital.
         </p>
         <p>
           Today, Televenture manages 4 Norsk Innovasjonskapital funds&nbsp;
-          <Link to="/Funds">(NIK I-IV)</Link>&nbsp; with a primary investment focus within Industrials, Software, Oil &
-          Gas, Marine, and Consumer sectors. Televenture’s management team have both long venture capital and general
-          industry experience both domestically and internationally, including a number of financial transactions such
-          as stock exchange listings and M&As.
+          <Link to="/Funds">(NIK I-IV)</Link>&nbsp; with a primary investment
+          focus within Industrials, Software, Oil & Gas, Marine, and Consumer
+          sectors. Televenture’s management team have both long venture capital
+          and general industry experience both domestically and internationally,
+          including a number of financial transactions such as stock exchange
+          listings and M&As.
         </p>
         <p>
-          The team members hold diversified competence and experience from a broad range of business areas. Special
-          competence areas include oil and gas, energy, IT and telecom. The team includes both entrepreneurs with proven
-          success, previous senior business leaders and persons with significant international operational experience,
-          including work in the US, and Asia the Middle-East.
+          The team members hold diversified competence and experience from a
+          broad range of business areas. Special competence areas include oil
+          and gas, energy, IT and telecom. The team includes both entrepreneurs
+          with proven success, previous senior business leaders and persons with
+          significant international operational experience, including work in
+          the US, and Asia the Middle-East.
         </p>
       </section>
       <section className={styles.teamWrapper}>
         {teamMembers.map((data, index) => (
-          <TeamMemberDetail key={`team-member-${data.id}`} index={index} {...data} />
+          <TeamMemberDetail
+            key={`team-member-${data.id}`}
+            index={index}
+            {...data}
+          />
         ))}
       </section>
+      <TeamMembersBackground className={styles.teamMembersBackground}>
+        <div className="imageOverlay" />
+      </TeamMembersBackground>
     </div>
   );
 };
 
-About.displayName = 'AboutSection';
+About.displayName = "AboutSection";
 export default About;
