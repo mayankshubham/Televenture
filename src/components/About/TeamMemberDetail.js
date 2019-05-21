57516c1269f89954605635843bbc88e2c0cf7811
Shubham Khatri <mayank.shubham94@gmail.com>
2019-05-21 13:47:21 +0530

    About page (#8)
    * about page layout
    
    * About page add message icon

---
+++ src/components/About/TeamMemberDetail.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import classNames from 'classnames';
+import Image from 'gatsby-image';
+import TeleventureIconGold from '../TeleventureIconGold';
+import MessageIcon from '../../images/messageIcon.svg';
+import styles from './teamMemberDetail.module.scss';
+
+const colors = [
+  {
+    backgroundColor: '#d2a756',
+    color: '#EEF3F6',
+  },
+  {
+    backgroundColor: '#b4cad6',
+    color: '#40667C',
+  },
+  {
+    backgroundColor: '#ffffff',
+    color: '#BC9B5D',
+  },
+  {
+    backgroundColor: '#72a0ba',
+    color: '#F9F9F9',
+  },
+];
+
+const TeamMemberDetail = props => {
+  const { name, designation, selfDescription, heroImage, index } = props;
+  const colorData = colors[index % 4];
+  return (
+    <div
+      className={classNames(styles.teamMemberContainer, {
+        [styles.left]: index % 2 === 0,
+        [styles.right]: index % 2 === 1,
+      })}
+    >
+      <div
+        style={{
+          backgroundColor: colorData.backgroundColor,
+          position: 'relative',
+        }}
+      >
+        <Image tag="div" fluid={heroImage.fluid} className={styles.image} />
+        <div className={styles.iconContainer}>
+          <a href={`mailto:post@televenture.no?subject=Til ${name}`}>
+            <img className={styles.messageIcon} src={MessageIcon} alt="" />
+          </a>
+          <TeleventureIconGold style={{ position: 'absolute' }} className={styles.televentureIcon} />
+        </div>
+        <div className={styles.description}>
+          <div className={styles.header}>
+            <h6 className={styles.name}>{name}</h6>
+            <h6 style={{ color: colorData.color }} className={styles.designation}>
+              {designation}
+            </h6>
+          </div>
+          <p className={styles.info}>{selfDescription.selfDescription}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMemberDetail;
