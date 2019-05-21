0dd48bd3cb90e6241d3d9fd8af7db6c8f51a329f
Shubham <shubhamkh@flock.com>
2019-05-21 23:11:26 +0530

    Added hover transitions on Fund cards

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Wrapper = styled.section`
   margin: 0 auto auto;
   width: 100%;
   flex-grow: 1;
   position: relative;
+  overflow: hidden;
   top: 82px;
-`
+`;
 
 const Container = props => {
-  return <Wrapper>{props.children}</Wrapper>
-}
+  return <Wrapper>{props.children}</Wrapper>;
+};
 
-export default Container
+export default Container;
