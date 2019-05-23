65f5b4164dd320cac7198ada0366ca65f03d18d0
Shubham <shubhamkh@flock.com>
2019-05-23 14:54:39 +0530

    Mobile sidebar buxfix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import Helmet from 'react-helmet'
-import favicon from '../images/favicon.ico'
-import GlobalStyle from '../styles/global'
-import theme from '../styles/theme'
-import config from '../utils/siteConfig'
-import Menu from '../components/Menu'
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Helmet from 'react-helmet';
+import favicon from '../images/favicon.ico';
+import GlobalStyle from '../styles/global';
+import theme from '../styles/theme';
+import config from '../utils/siteConfig';
+import Menu from './Menu';
 
 const Layout = ({ children }) => {
   return (
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
       <Helmet>
         <title>{config.siteTitle}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width" />
         <link rel="icon" href={favicon} />
       </Helmet>
 
@@ -27,7 +27,7 @@ const Layout = ({ children }) => {
       </ThemeProvider>
       <GlobalStyle />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
