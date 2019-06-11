9cd2d029921087d01264c5925ae50383b3679c6c
Shubham Khatri <mayank.shubham94@gmail.com>
2019-06-11 23:51:10 +0530

    Final deployment (#36)
    * Adding contentful.json and updating fund
    
    * Mobile screen parallax issue + other additions

--- src/components/News/HeaderBackground.js
+++ src/components/News/HeaderBackground.js
@@ -16,7 +16,6 @@ const BackgroundSection = ({ className, children, style = {}, tag = 'section', b
     }
   `);
   const imageData = data.desktop.childImageSharp.fluid;
-  console.log({ imageData });
   return (
     <BackgroundImage
       Tag={tag}
